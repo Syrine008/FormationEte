a43166066b243fb36395348652c2e0ca094afd42
Syrine008 <sirine.ghorbel29@gmail.com>
2024-07-17 21:34:49 +0100

    add validation for signup form

--- app/src/main/java/com/example/mysecondapplication/SignUpActivity.java
+++ app/src/main/java/com/example/mysecondapplication/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.mysecondapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +13,72 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
 
-    private TextView goTosignIn;
+    //declaration de variables
+    private TextView goToSignIn;
+    private EditText fullName, email, cin, phone, password, confirmPassword;
+    private Button btnSignUp;
+    private String fullNameString, emailString, cinString, phoneString, passwordString, confirmPasswordString;
+    private final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
-        goTosignIn = findViewById(R.id.goTosignIn);
-        goTosignIn.setOnClickListener(v -> {
-            startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+        //affectation des views
+        goToSignIn = findViewById(R.id.goTosignIn);
+        fullName = findViewById(R.id.fullNameSignUp);
+        email = findViewById(R.id.emailSignUp);
+        cin = findViewById(R.id.cinSignUp);
+        phone = findViewById(R.id.phoneSignUp);
+        password = findViewById(R.id.passwordSignUp);
+        confirmPassword = findViewById(R.id.confirmPasswordSignUp);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        //actions
+        goToSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
         });
+
+        btnSignUp.setOnClickListener(v -> {
+            if (validate()){
+                Toast.makeText(this, "Done !", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //validation des données
+    private boolean validate() {
+
+        boolean result = false;
+        fullNameString = fullName.getText().toString().trim();
+        emailString = email.getText().toString().trim();
+        cinString = cin.getText().toString().trim();
+        phoneString = phone.getText().toString().trim();
+        passwordString = password.getText().toString().trim();
+        confirmPasswordString = confirmPassword.getText().toString().trim();
+
+        if (fullNameString.length() < 7) {
+            fullName.setError("fullname invalide !");
+        } else if (!isValidPattern(emailString, EMAIL_PATTERN)) {
+            email.setError("Email invalide !!");
+
+        } else if (cinString.length() < 8) {
+
+        }else
+            result = true ;
+        return result;
+    }
+
+    private boolean isValidPattern(String mot, String patternn) {
+        Pattern pattern = Pattern.compile(patternn);
+        Matcher matcher = pattern.matcher(mot);
+        return matcher.matches();
     }
 }
